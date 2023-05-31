4ec6c50102022ca7ff952475523b657b9f8f4155
aloulou1238 <benhibaali@gmail.com>
2023-05-31 12:20:46 +0200

    Deleted phonebook app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import AppPersons from "./components/persons/AppPersons"
 import CountriesApp from "./components/Countries/CountriesApp"
 
 const App = () => {
   return (
-    <AppPersons />
-    // <CountriesApp />
+    <CountriesApp />
   )
 }
 
